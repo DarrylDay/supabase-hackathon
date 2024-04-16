d2e430b65f93ced2d1c3e3dabaed36ff4b5fc569
Darryl Day <darrylday16@gmail.com>
2024-04-16 11:11:59 -0700

    Added vercel analytics and speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
-import { supabase } from '@/lib/initSupabase'
-import '@/styles/app.css'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import type { AppProps } from 'next/app'
+import { supabase } from "@/lib/initSupabase";
+import "@/styles/app.css";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <SessionContextProvider supabaseClient={supabase}>
-      <Component {...pageProps} />
-    </SessionContextProvider>
-  )
+	const router = useRouter();
+
+	return (
+		<SessionContextProvider supabaseClient={supabase}>
+			<Component {...pageProps} />
+			<SpeedInsights route={router.pathname} />
+			<Analytics />
+		</SessionContextProvider>
+	);
 }
